a34f42fff0706fa6c9458193c89445bb687b2e7c
Igor_Fedyukin <igor_fedyukin@epam.com>
2018-06-17 00:08:05 +0300

    Add css-class for chapter

--- book/toggle.js
+++ book/toggle.js
@@ -2,17 +2,21 @@ require(["gitbook", "jQuery"], function(gitbook, $) {
 
   function expand(chapter) {
     chapter.show();
-
-    if (chapter.parent().attr('class') != 'summary' &&
-      chapter.parent().attr('class') != 'book-summary' &&
-      chapter.length != 0
-    ) {
-      expand(chapter.parent());
-    }
+    if (chapter.parent().attr('class') != 'summary'
+        && chapter.parent().attr('class') != 'book-summary'
+      && chapter.length != 0
+       ) {
+         chapter.children('a').first().addClass('chapter-top--open');
+         expand(chapter.parent());
+       }
   }
 
   gitbook.events.bind("page.change", function() {
     $('li.chapter').children('ul.articles').hide();
+    $('li.chapter').has('ul.articles').each(function(item) {
+      $(this).children('a').first().addClass('chapter-top');
+    })
+
     $chapter = $('li.chapter.active');
     $children = $chapter.children('ul.articles');
     $parent = $chapter.parent();
